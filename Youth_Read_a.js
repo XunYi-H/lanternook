2608089aec894cbbd93cfac410049b1d4aedf6d6
lanternook <89370333+lanternook@users.noreply.github.com>
2021-09-03 09:00:51 +0800

    Update and rename raw_Youth_Read.js to Youth_Read_a.js

--- Youth_Read_a.js
+++ Youth_Read_a.js
@@ -5,7 +5,8 @@ Github Actions使用方法见[@lxk0301](https://raw.githubusercontent.com/lxk030
 点击几篇文章和视频，自动获取阅读请求，在Github Actions中的Secrets新建name为'YOUTH_READ'的一个值，拷贝抓包的请求体到下面Value的文本框中，添加的请求体越多，获得青豆次数越多，本脚本不包含任何推送通知
 
 多个请求体时用'&'号或者换行隔开" ‼️
-
+######
+安卓版中青阅读
 */
 
 const $ = new Env("中青看点阅读")
