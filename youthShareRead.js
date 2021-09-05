2e605498ee6b497025b850514065ef8b9590b80d
lanternook <89370333+lanternook@users.noreply.github.com>
2021-09-05 15:48:48 +0800

    Update youthShareRead.js

--- youthShareRead.js
+++ youthShareRead.js
@@ -14,10 +14,10 @@ https://gitee.com/curtinlv/qx/raw/master/rewrite/youth.conf, tag=中青 by Curti
 const $ = new Env("中青分享阅读-助力10次");
 $.idx = ($.idx = ($.getval('zqSuffix') || '1') - 1) > 0 ? ($.idx + 1 + '') : ''; // 账号扩展字符
 //自定义分享次数
-let min = $.getdata('zqsharemin') || 1; //分享最少次数
-let max = $.getdata('zqsharemax') || 5; //分享最多次数
+//let min = $.getdata('zqsharemin') || 1; //分享最少次数
+let sharemax = $.getdata('zqsharemax') || 3; //分享最多次数
 //随机生成分享次数 
-let rand = Math.floor(Math.random()*(max-min+1))+min;
+//let rand = Math.floor(Math.random()*(max-min+1))+min;
 
 if ($request) getShareInfo();
 
@@ -30,8 +30,8 @@ async function getShareInfo() {
       	if (url) $.setdata(url,'shareurl_zq'+ $.idx);
       	console.log("url:" + url);
       	console.log("s_si:" + s_si);
-      	$.msg("中青分享\n", "", `数据获取成功\n还剩${rand}次.`);
-      	for(let i=1;i<rand;i++){
+      	$.msg("中青分享\n", "", `数据获取成功\n还剩${sharemax}次.`);
+      	for(let i=1;i<sharemax;i++){
 		//await $.wait(8000+Math.floor(5000 * Math.random()));
         	DD = 8000+Math.floor(5000 * Math.random());
         	console.log(`随机延迟${DD/1000}秒`);
@@ -41,7 +41,7 @@ async function getShareInfo() {
         	//await postShareInfoa(url,s_si, i)
       	}
 	  
-	$.msg("中青分享\n", "", `数据分享${rand}次完成.`);
+	$.msg("中青分享\n", "", `数据分享${sharemax}次完成.`);
 	$.done();
       } else {
         $.notify("中青分享", "", "️url获取失败");
